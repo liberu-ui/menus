d00806dfafb2e72d991e47c17ad5caaf6150a2b8
mishu <mishu@xtelecom.ro>
2022-01-17 11:27:08 +0200

    updates settings registration

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -1,4 +1,4 @@
 import App from '@enso-ui/ui/src/core/app';
 import MenuOrganizer from './components/settings/MenuOrganizer.vue';
 
-App.registerSettingsItem('setting-menu-organizer', MenuOrganizer, 400);
+App.registerSettingsItem('menu-organizer', MenuOrganizer, 400, 'system.menus.organize');
