bcf8eb61e0099d44d15862457df906c59dcf227d
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-06 16:02:16 +0000

    Replace enso-ui to liberu-ui

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -1,4 +1,4 @@
-import App from '@enso-ui/ui/src/core/app';
+import App from '@liberu-ui/ui/src/core/app';
 import MenuOrganizer from './components/settings/MenuOrganizer.vue';
 
 App.registerSettingsItem('menu-organizer', MenuOrganizer, 400, 'system.menus.organize');
