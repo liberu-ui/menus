ffc8f432bd448d475161785bf81f701cf3cf1a0c
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 20:40:25 +0000

    Replace JavaScript with TypeScript

--- src/bulma/register.js
+++
@@ -1,4 +0,0 @@
-import App from '@liberu-ui/ui/src/core/app';
-import MenuOrganizer from './components/settings/MenuOrganizer.vue';
-
-App.registerSettingsItem('menu-organizer', MenuOrganizer, 400, 'system.menus.organize');
